ca186f2a69934248079b97e2f1cb0ebe527fdcb6
andrei-ott <andrei.ott@docusketch.com>
2024-11-21 22:10:17 +0400

    add asteroid destruction

--- main.py
+++ main.py
@@ -51,6 +51,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.is_collided(shot):
+                    asteroid.kill()
+                    shot.kill()
+                    break
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
